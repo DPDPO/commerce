e225fc9f624036400e1edc2d9dcd92f77f260d1b
psc <whirled7@gmail.com>
2023-05-09 07:45:43 +0900

    style setting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
+/** @jsxImportSource @emotion/react */
+
 import Head from "next/head";
 import Image from "next/image";
 // import { Inter } from 'next/font/google'
 import styles from "../styles/Home.module.css";
 import { useEffect, useRef, useState } from "react";
+import { css } from "@emotion/react";
 
 // const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +13,9 @@ export default function Home() {
   // const [products, setProducts] = useState<
   //   { id: string; properties: { id: string }[] }[]
   // >([]);
-  const [products, setProducts] = useState<{ id: string; name: string }[]>([]);
+  const [products, setProducts] = useState<
+    { id: string; name: string; createdAt: string }[]
+  >([]);
   // useEffect(() => {
   //   fetch("/api/get-items")
   //     .then((res) => res.json())
@@ -28,14 +33,15 @@ export default function Home() {
   const handleClick = () => {
     if (inputRef.current === null || inputRef.current.value === "") {
       alert("name을 입력해주세요.");
+    } else {
+      fetch(`/api/add-item?name=${inputRef.current?.value}`)
+        .then((res) => res.json())
+        .then(
+          (data) => alert(data.message)
+          //  console.log(data)
+        );
+      // .then((res) => console.log(res));
     }
-    fetch(`/api/add-item?name=${inputRef.current?.value}`)
-      .then((res) => res.json())
-      .then(
-        (data) => alert(data.message)
-        //  console.log(data)
-      );
-    // .then((res) => console.log(res));
   };
 
   return (
@@ -52,14 +58,33 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className={styles.code}>pages/index.tsx</code>
         </p>
-        <input ref={inputRef} type="text" placeholder="name" />
-        <button onClick={handleClick}>Add jaket</button>
+        <input
+          className="placeholder:italic placeholder:text-slate-400 block bg-white w-96 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          placeholder="Search for anything..."
+          type="text"
+          name="search"
+          ref={inputRef}
+        />
+        <button
+          css={css`
+            padding: 32px;
+            background-color: hotpink;
+            font-size: 24px;
+            border-radius: 4px;
+          `}
+          onClick={handleClick}
+        >
+          Add jaket
+        </button>
         <div>
           <p>Product List</p>
           {products &&
             products.map((item) => (
               <>
                 <div key={item.id}>{item.name}</div>
+                <span>{item.createdAt}</span>
+                <br />
+                <br />
               </>
             ))}
 
