e94d008969f1293734d199d7a5daa1f3c15246b1
psc <whirled7@gmail.com>
2023-05-14 08:31:28 +0900

    fix

--- pages/api/get-products.ts
+++ pages/api/get-products.ts
@@ -3,7 +3,11 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-async function getProducts(skip: number, take: number, category: number) {
+async function getProducts(
+  skip: number,
+  take: number,
+  category: number | null
+) {
   const where =
     category && category !== -1
       ? {
