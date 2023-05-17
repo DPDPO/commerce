d9fe9788491be2055d61f7e4c40e3694e0c6f09e
psc <whirled7@gmail.com>
2023-05-17 09:04:03 +0900

    feat : search

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 // import '@/styles/globals.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: { staleTime: Infinity },
+    },
+  });
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
--- pages/products/page.tsx
+++ pages/products/page.tsx
@@ -1,37 +1,89 @@
 import { categories, products } from "@prisma/client";
 import Image from "next/image";
 import { useCallback, useEffect, useState } from "react";
-import { Pagination, SegmentedControl } from "@mantine/core";
-import { CATECORY_MAP, TAKE } from "constants/products";
+import { Input, Pagination, SegmentedControl, Select } from "@mantine/core";
+import { CATECORY_MAP, FILTERS, TAKE } from "constants/products";
+import { IconSearch } from "@tabler/icons-react";
+import useDebounce from "hooks/useDebounce";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Products() {
   const [activePage, setPage] = useState(1);
   const [total, setTotal] = useState(0);
   const [categories, setCategories] = useState<categories[]>([]);
-  const [products, setProducts] = useState<products[]>([]);
+  // const [products, setProducts] = useState<products[]>([]); useQuery product때문에 주석
   const [selectCategory, setSelectCategory] = useState<string>("-1");
+  const [selectedFilter, setSelectedFilter] = useState<string | null>(
+    FILTERS[0].value
+  );
+  const [keyword, setKeyword] = useState("");
+  const debouncedKeyword = useDebounce<string>(keyword);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+  };
+
   useEffect(() => {
     fetch(`/api/get-categories`)
       .then((res) => res.json())
       .then((data) => setCategories(data.items));
   }, []);
   useEffect(() => {
-    fetch(`/api/get-products-count?category=${selectCategory}`)
-      .then((res) => res.json())
-      .then((data) => setTotal(Math.ceil(data.items / TAKE)));
-  }, [selectCategory]);
-
-  useEffect(() => {
-    const skip = TAKE * (activePage - 1);
     fetch(
-      `/api/get-products?skip=${skip}&take=${TAKE}&category=${selectCategory}`
+      // `/api/get-products-count?category=${selectCategory}&contains=${keyword}` => 매입력마다 불필요한 랜더링 때문에 debounce해줌 밑에도 마찬가지
+      `/api/get-products-count?category=${selectCategory}&contains=${debouncedKeyword}`
     )
       .then((res) => res.json())
-      .then((data) => setProducts(data.items));
-  }, [activePage, selectCategory]);
+      .then((data) => setTotal(Math.ceil(data.items / TAKE)));
+  }, [selectCategory, /*keyword */ debouncedKeyword]);
+
+  // useEffect(() => {
+  //   const skip = TAKE * (activePage - 1);
+  //   fetch(
+  //     `/api/get-products?skip=${skip}&take=${TAKE}&category=${selectCategory}&orderBy=${selectedFilter}&contains=${debouncedKeyword}`
+  //   )
+  //     .then((res) => res.json())
+  //     .then((data) => setProducts(data.items));
+  // }, [activePage, selectCategory, selectedFilter, debouncedKeyword]);
+
+  const { data: products } = useQuery<
+    { items: products[] },
+    unknown,
+    products[]
+  >(
+    [
+      `/api/get-products?skip=${
+        TAKE * (activePage - 1)
+      }&take=${TAKE}&category=${selectCategory}&orderBy=${selectedFilter}&contains=${debouncedKeyword}`,
+    ],
+    () =>
+      fetch(
+        `/api/get-products?skip=${
+          TAKE * (activePage - 1)
+        }&take=${TAKE}&category=${selectCategory}&orderBy=${selectedFilter}&contains=${debouncedKeyword}`
+      ).then((res) => res.json()),
+    {
+      select: (data) => data.items,
+    }
+  );
 
   return (
     <div className="px-36 mt-36 mb-36">
+      <div className="mb-4">
+        <Input
+          icon={<IconSearch />}
+          placeholder="Search"
+          value={keyword}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="mb-4">
+        <Select
+          value={selectedFilter}
+          onChange={setSelectedFilter}
+          data={FILTERS}
+        />
+      </div>
       {categories && (
         <div className="mb-4">
           <SegmentedControl
@@ -48,7 +100,6 @@ export default function Products() {
           />
         </div>
       )}
-
       {products && (
         <div className="grid grid-cols-3 gap-5">
           {products.map((item) => (
