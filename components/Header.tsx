5a073734d312be517d9d68f500f1a9622e1844f6
psc <whirled7@gmail.com>
2023-05-26 07:47:14 +0900

    main page

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,39 +12,43 @@ export default function Header() {
   const { data: session } = useSession();
   const router = useRouter();
   return (
-    <div className="mt-12 mb-12">
-      <div className="w-full flex h-50 items-center">
-        <IconHome2 onClick={() => router.push("/")} />
-        <span className="m-auto" />
-        {session ? (
-          <>
-            {/* <image
+    <>
+      {router.pathname === "/" ? undefined : (
+        <div className="mt-12 mb-12">
+          <div className="w-full flex h-50 items-center">
+            <IconHome2 onClick={() => router.push("/shop")} />
+            <span className="m-auto" />
+            {session ? (
+              <>
+                {/* <image
                 // alt="user"
                 // src={session.user?.image}
                 width={30}
                 height={30}
                 style={{ borderRadius: "50%" }}
             /> */}
-            <IconHeart
-              className="mr-4"
-              onClick={() => router.push("/wishlist")}
-            />
-            <IconShoppingCart
-              className="mr-4"
-              onClick={() => router.push("/cart")}
-            />
-            <IconUserCircle
-              // className="mr-12"
-              onClick={() => router.push("/my")}
-            />
-          </>
-        ) : (
-          <IconUser
-            // className="mr-12"
-            onClick={() => router.push("/auth/login")}
-          />
-        )}
-      </div>
-    </div>
+                <IconHeart
+                  className="mr-4"
+                  onClick={() => router.push("/wishlist")}
+                />
+                <IconShoppingCart
+                  className="mr-4"
+                  onClick={() => router.push("/cart")}
+                />
+                <IconUserCircle
+                  // className="mr-12"
+                  onClick={() => router.push("/my")}
+                />
+              </>
+            ) : (
+              <IconUser
+                // className="mr-12"
+                onClick={() => router.push("/auth/login")}
+              />
+            )}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
